d8e1cd0161d60aa90093f995f57e53304f08e028
Ziyan Ali Murtaza <ziyanali6@gmail.com>
2024-09-30 06:24:08 +0500

    update to base sample.mp4 added

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-   plugins: [react()],
-   base: '/ibexProjectH/'
-})
+  base: '/ibexProjectH/', // Ensure this matches your repository name
+  // other configurations...
+});
