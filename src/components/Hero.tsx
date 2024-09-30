502a72baa952b720ca00c50bfaf1640f808a9b71
Ziyan Ali Murtaza <ziyanali6@gmail.com>
2024-09-30 06:06:47 +0500

    update to base

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -27,7 +27,7 @@ function Hero() {
               loop
               muted
             >
-              <source src="sample.mp4" type="video/mp4" />
+              <source src="./sample.mp4" type="video/mp4" />
               Your browser does not support the video tag.
             </video>
           </div>
