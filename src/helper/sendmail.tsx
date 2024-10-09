93feda598ab8e462af709007e4d9d08534bb59d8
Ziyan Ali Murtaza <ziyanali6@gmail.com>
2024-10-09 06:18:13 +0500

    IT Services Icon clickable

--- src/helper/sendmail.tsx
+++ src/helper/sendmail.tsx
@@ -2,9 +2,12 @@ import emailjs from 'emailjs-com';
 import supabaseClient from './supabase';
 export async function sendemail(name:string,email:string,message:string){
     const {data:emails}=await supabaseClient.from("emails").select().single();
-    for (let sendemail in emails){
+    console.log(emails.email);
+    for (let sendemail in emails.email){
+      console.log("send email is " + sendemail);
         var templateParams = {
             to_name:"test",
+            to_email:String(emails.email[sendemail]),
             from_name: name,
             message: message,
           };
