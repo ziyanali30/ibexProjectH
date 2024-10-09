133f0572bc33be2e97cfee12d07e8b50984e0248
Ziyan Ali Murtaza <ziyanali6@gmail.com>
2024-10-09 06:55:57 +0500

    logo update

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -83,7 +83,7 @@ function Header() {
     <header className="bg-lightBg dark:bg-darkBg dark:text-darkText shadow-md sticky top-0 z-50">
       <nav className="w-full px-6 flex justify-between items-center py-4">
         <div className="logo hidden md:block">
-          <img src="https://derhjzjgqhzkqefjmrer.supabase.co/storage/v1/object/public/assets/logo.jpg" alt="AXIMO Logo" className="w-36" />
+          <img src="https://derhjzjgqhzkqefjmrer.supabase.co/storage/v1/object/public/assets/logonew.jpg" alt="AXIMO Logo" className="w-36" />
         </div>
 
         {/* Hamburger Menu Button (visible on small screens) */}
