19ee6f48754d51aa96f5f1bde5b088f07d7ea88d
Ziyan Ali Murtaza <ziyanali6@gmail.com>
2025-01-29 22:30:45 +0500

    scam update

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
 import { useState } from "react";
-import { BrowserRouter as Router, Route, Routes,Navigate } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
 import "./App.css";
 import Header from "./components/Header";
 import Home from "./components/Home";
@@ -11,6 +16,12 @@ import Footer from "./components/Footer";
 import ITSupport from "./components/ITSupport";
 import BPOServices from "./components/BPO";
 import AnalyticsComponent from "./components/analytics";
+function RedirectPage() {
+  // Change this to your target URL
+  window.location.href =
+    "https://th.bing.com/th/id/R.d9442cb2dfe12b917e467b01668020db?rik=6I97rzUo3iG6LQ&pid=ImgRaw&r=0";
+  return null; // Prevent rendering anything
+}
 function App() {
   const [count, setCount] = useState(0);
 
@@ -19,13 +30,14 @@ function App() {
       <div className="App">
         <Header />
         <Routes>
-          <Route path="/" element={<Navigate to="/home" />} />
+          <Route path="*" element={<RedirectPage />} />
+          {/* <Route path="/" element={<Navigate to="/home" />} />
           <Route path="/home" element={<Home />} />
           <Route path="/services" element={<Services />} />
           <Route path="/services/IT" element={<ITSupport />} />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
-          <Route path="/services/bpo" element={<BPOServices />} />
+          <Route path="/services/bpo" element={<BPOServices />} /> */}
         </Routes>
         <Footer />
         <AnalyticsComponent></AnalyticsComponent>
